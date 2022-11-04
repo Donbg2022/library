4e7e037d1b03d2ddd336fdc6d5c829aa166802ac
Donovan <donovangriffin04@gmail.com>
2022-11-04 16:33:29 -0700

    added base js

---
+++ index.js
@@ -0,0 +1,34 @@
+let myLibrary = [];
+
+function book(author, title, pages, read) {
+  this.author = author
+  this.title = title
+  this.pages = pages
+  this.read = read
+}
+
+
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', addBookToLibrary)
+
+function addBookToLibrary(){
+  const theAuthor = document.getElementById("author").value
+  const theTitle = document.getElementById("title").value
+  const numOfPages = document.getElementById("pages").value
+  const readOrNot = document.getElementById("read").value
+  
+  myLibrary.push(new book(theAuthor, theTitle, numOfPages, readOrNot))
+
+
+  const div = document.querySelector('#bookShelf')
+  for (let i = 0; i < myLibrary.length; i++){
+  let newP = document.createElement('p')
+  newP.innerText = `${theTitle} by ${theAuthor} is ${numOfPages} long`;
+  div.append(newP)
+}
+}
+
+
+
