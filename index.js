29207b86156b99d4432f365ae860d40ed426c830
Donovan <donovangriffin04@gmail.com>
2022-11-04 17:25:30 -0700

    added javascript buttons to open and close popups

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ function book(author, title, pages, read) {
 
 
 
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', addBookToLibrary)
-//add book to library when inputs are filled
-function addBookToLibrary(){
   const theAuthor = document.getElementById("author").value
   const theTitle = document.getElementById("title").value
   const numOfPages = document.getElementById("pages").value
   const readOrNot = document.getElementById("read").value
+  const btn = document.querySelector('#btn');
+btn.addEventListener('click', addBookToLibrary)
+
+
+//add book to library when inputs are filled
+function addBookToLibrary(e){
+  //prevents reloading
+  e.preventDefault
+
   
   myLibrary.push(new book(theAuthor, theTitle, numOfPages, readOrNot))
 
@@ -27,10 +31,25 @@ function addBookToLibrary(){
   let newP = document.createElement('p')
   newP.innerText = `${theTitle} by ${theAuthor} is ${numOfPages} long`;
   div.append(newP)
+
+  
 }
 
 
+//when book is added the form goes away
+const x = document.querySelector('#x')
+x.addEventListener('click', function(){
+  formWrapper.style.display = 'none'
+})
+  
+
 //popup the form when button is clicked
+const popupBtn = document.querySelector('#add-book')
+const formWrapper = document.querySelector('form')
+popupBtn.addEventListener('click', function(){
+  formWrapper.style.display = 'block'
+  console.log('clicked')
+})
 
 
 
